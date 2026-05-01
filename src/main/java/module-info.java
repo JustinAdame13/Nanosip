caacaa4d60d77020a5abb5d16f730c518fc9cf45
JustinAdame13 <justinlool13@gmail.com>
2026-04-30 18:31:01 -0600

    se crearon los modales de clientes y proveedores

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,10 @@ module mx.nanosip.nanosip {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens mx.nanosip.nanosip to javafx.fxml;
     exports mx.nanosip.nanosip;
     exports mx.nanosip.nanosip.Controllers;
     opens mx.nanosip.nanosip.Controllers to javafx.fxml;
+    exports mx.nanosip.nanosip.Controllers.Modals;
+    opens mx.nanosip.nanosip.Controllers.Modals to javafx.fxml;
 }
---
+++ src/main/java/mx/nanosip/nanosip/Controllers/Modals/ModalController.java
@@ -0,0 +1,7 @@
+package mx.nanosip.nanosip.Controllers.Modals;
+
+import javafx.stage.Stage;
+
+public interface ModalController {
+    void setModalStage(Stage stage);
+}
